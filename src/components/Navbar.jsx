90e63b8d1b0c55c88ebe3fc1f667f9328ba07503
boomieindahouse <prawitsanat@hotmail.com>
2024-10-22 04:15:13 +0700

    edit nav background

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,20 @@
 import { Link } from 'react-router-dom';
 
 export default function Navbar() {
+    const navStyle = {
+        backgroundColor: "#F4EAE0",
+    };
+
     return (
-        <div className="navbar bg-base-100">
+        <div className="navbar bg-base-100 text-black" style={navStyle}>
             <div className="flex-1">
                 <a className="btn btn-ghost text-xl">daisyUI</a>
             </div>
             <div className="flex-none">
                 <ul className="menu menu-horizontal px-1">
-                    <li><a>Link</a></li>
-                    <li>
-                        <details>
-                            <summary>Parent</summary>
-                            <ul className="bg-base-100 rounded-t-none p-2">
-                                <li><Link to="/home">Home</Link></li>
-                                <li><Link to="/product">Link 2</Link></li>
-                            </ul>
-                        </details>
-                    </li>
+                    <li><Link to="/home">Home</Link></li>
+                    <li><Link to="/home">Home</Link></li>
+                    <li><Link to="/home">Home</Link></li>
                 </ul>
             </div>
         </div>
